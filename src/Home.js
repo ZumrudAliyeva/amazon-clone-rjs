6ca5f112522eea51338f8772abd7bc0951c4d952
ZumrudAliyeva <zumrudka@code.edu.az>
2021-08-26 18:26:10 +0400

    builded product component

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Home.css'
+import Product from './Product'
 
 function Home() {
     return (
         <div className="home">
-            <img className="home__image" src="https://m.media-amazon.com/images/I/61CiqVTRBEL._SX3000_.jpg" alt="banner" />
+            <img className="home__image" src="https://m.media-amazon.com/images/I/61CiqVTRBEL._SX3000_.jpg" 
+            alt="banner" />
+            <div className="home__row">
+            <Product 
+            id={12345}
+            title="lorem ipsum dolor gfxhgcjhvkhvkhvkj" 
+            price={546.00} 
+            rating={5} 
+            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/July/amazonbasics_520x520._SY304_CB442725065_.jpg"/>
+            </div>
         </div>
     )
 }
