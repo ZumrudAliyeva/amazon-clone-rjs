e4e4c221e4ca8c2e9e3402aecc88e23f46157f71
ZumrudAliyeva <zumrudka@code.edu.az>
2021-08-26 16:44:41 +0400

    added simple banner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
     <div className="app">
     <Switch>
-      <Route path="/checkout"></Route>
+      <Route path="/checkout">
+      <Header/>
+
+      </Route>
       <Route path="/login"></Route>
       <Route path="/">
         <Header/>
+        <Home/>
       </Route>
     </Switch>
     </div>
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <img className="home__image" src="https://m.media-amazon.com/images/I/61CiqVTRBEL._SX3000_.jpg" alt="banner" />
+        </div>
+    )
+}
+
+export default Home
